50df969340b54a752072664bf0a492aeb647f991
pratikb99 <50937293+pratikb99@users.noreply.github.com>
2020-06-16 18:04:13 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,7 +13,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
+    await alive.edit("**Apun Zinda He Surr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
                      "`Indian Bot Version:` [1.0](https://telegra.ph/INDIAN-06-15-6)\n`Python:` **3.7.4**\n"
                      "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
